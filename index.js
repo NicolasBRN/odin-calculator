469b0c182a560bcf0e628dab00c696c8f8488936
Brandon <nicolas.brn@hotmail.com>
2022-12-05 23:08:24 -0300

    Upgrade calculator so now can operate with the same operator over and over

--- index.js
+++ index.js
@@ -21,10 +21,16 @@ function operate (a, b, operator) {
 }
 
 const display = document.getElementById("display")
+
 const numbers = document.querySelectorAll(".numbers")
 numbers.forEach(function (number) {
     number.addEventListener('click', function () {
         display.value += number.textContent;
+        if (autoclean === 1) {
+            display.value = "";
+            display.value = number.textContent;
+            autoclean--;
+        }
     })
 })
 
@@ -34,24 +40,38 @@ clean.addEventListener('click', function () {
 })
 
 let operator;
-let a;
+let a = 0;
+let b = 0;
+let click = 0;
+let autoclean = 0;
 
 const operators = document.querySelectorAll(".operators")
 operators.forEach(function (oper) {
     oper.addEventListener('click', function () {
-        
-
-        a = display.value;
-        display.value = "";
-
-        operator = oper.textContent
-        console.log(operator)
+        click++;
+        operator = oper.textContent;
+        oneOrMoreOperations();
     })
 })
 
+function oneOrMoreOperations () {
+    if (click >= 2) {
+        a = operate(a, display.value,operator);
+        display.value = "";
+        display.value = a
+        autoclean++;
+    }
+
+    else {
+        a = display.value;
+        display.value = "";
+    }
+
+}
+
 const equal = document.getElementById("equal")
 equal.addEventListener('click', function() {
-    let b = display.value;
+    b = display.value;
     display.value = operate(a, b, operator);
 })
 
