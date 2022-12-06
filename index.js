6794cec46dd612e93be15aaea735b76d7957f6a0
Brandon <nicolas.brn@hotmail.com>
2022-12-06 16:34:01 -0300

    Update functions, calculator now working with dif operators

--- index.js
+++ index.js
@@ -1,77 +1,80 @@
-function add (a, b) {
-    return +a + +b;
-}
-function subtract (a, b) {
-    return a - b;
-}
-function multiply (a, b) {
-    return a * b;
-}
-function divide (a, b) {
-    return a / b;
-}
+// crear funcion que permita usar los operadores basicos + - / *
 
-function operate (a, b, operator) {
+function operate (firstValue, secondValue, operator) {
     switch (operator) {
-        case "+": return add(a,b);
-        case "-": return subtract(a,b);
-        case "*": return multiply(a,b);
-        case "/": return divide(a,b);
+        case "+": return firstValue + secondValue;
+        case "-": return firstValue - secondValue;
+        case "*": return firstValue * secondValue;
+        case "/": return firstValue / secondValue;
     }
 }
 
+// al usar los numeros, que se vean en el display
 const display = document.getElementById("display")
-
 const numbers = document.querySelectorAll(".numbers")
 numbers.forEach(function (number) {
     number.addEventListener('click', function () {
         display.value += number.textContent;
-        if (autoclean === 1) {
+        value += number.textContent;
+    }
+)})
+    
+const clean = document.getElementById("clean")
+    clean.addEventListener('click', function () {
+        display.value = "";
+        value = 0;
+        operator = "";
+        firstValue = undefined;
+        secondValue;
+})
+
+let value = 0;
+let operator = "";
+let firstValue;
+let secondValue;
+
+
+const operators = document.querySelectorAll(".operators");
+operators.forEach(function (op) {
+    op.addEventListener('click', function () {
+        if (firstValue === undefined) {
+            operator = op.textContent;
+            firstValue = value;
+            value = 0;
+            display.value += op.textContent;
+        } 
+        else if (value === 0) {
+            value = firstValue;
+            operator = op.textContent;
+            display.value = firstValue + op.textContent;
+            value = 0;
+            return;
+        }
+        else {
             display.value = "";
-            display.value = number.textContent;
-            autoclean--;
+            secondValue = value;
+            value = 0;
+            firstValue = operate(+firstValue, +secondValue, operator);
+            operator = op.textContent;
+            console.log(firstValue)
+            display.value = firstValue + operator;
         }
     })
 })
 
-const clean = document.getElementById("clean")
-clean.addEventListener('click', function () {
-    display.value = "";
-})
+const equal = document.querySelector("#equal");
+equal.addEventListener('click', function () {
 
-let operator;
-let a = 0;
-let b = 0;
-let click = 0;
-let autoclean = 0;
-
-const operators = document.querySelectorAll(".operators")
-operators.forEach(function (oper) {
-    oper.addEventListener('click', function () {
-        click++;
-        operator = oper.textContent;
-        oneOrMoreOperations();
-    })
-})
-
-function oneOrMoreOperations () {
-    if (click >= 2) {
-        a = operate(a, display.value,operator);
-        display.value = "";
-        display.value = a
-        autoclean++;
+    if (firstValue === undefined) {
+        display.value = "ERROR";
     }
-
     else {
-        a = display.value;
-        display.value = "";
+        secondValue = value;
+        value = 0;
+        firstValue = operate(+firstValue, +secondValue, operator);
+        display.value = firstValue;
+        secondValue = 0;
     }
-
-}
-
-const equal = document.getElementById("equal")
-equal.addEventListener('click', function() {
-    b = display.value;
-    display.value = operate(a, b, operator);
 })
 
+// poder combinar los operadores al realizar la operacion
